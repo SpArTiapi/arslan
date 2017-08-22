7b8e1b02becd4e30341599b1e7c19b16827e0a20
KafTeam <31245237+KafTeam@users.noreply.github.com>
2017-08-22 18:50:14 +0430

    Update kaf.sh

--- kaf.sh
+++ kaf.sh
@@ -19,77 +19,7 @@ install() {
 		chmod +x tgcli
 		cd ..
 		chmod +x bot
-}
-
-function print_logo() {
-	green ""
-	green ""
-	green ""
-	green ""
-	echo -e "\n\e[0m"
-}
-
-function logo_play() {
-    declare -A txtlogo
-    seconds="0.010"
-    txtlogo[1]=""
-    txtlogo[2]=""
-    txtlogo[3]=""
-    txtlogo[4]=""
-    printf "\e[31m\t"
-    for i in ${!txtlogo[@]}; do
-        for x in `seq 0 ${#txtlogo[$i]}`; do
-            printf "${txtlogo[$i]:$x:1}"
-            sleep $seconds
-        done
-        printf "\n\t"
-    done
-    printf "\n"
-	echo -e "\e[0m"
-}
-
-function beyondteam() {
-	echo -e "\e[0m"
-	green ""
-	green ""
-	white ""
-	white ""
-	red   ""
-	red   ""
-	echo -e "\e[0m"
-}
-
-red() {
-  printf '\e[1;31m%s\n\e[0;39;49m' "$@"
-}
-green() {
-  printf '\e[1;32m%s\n\e[0;39;49m' "$@"
-}
-white() {
-  printf '\e[1;37m%s\n\e[0;39;49m' "$@"
-}
-update() {
-	git pull
-}
-
-if [ "$1" = "install" ]; then
-	print_logo
-	kaf
-	logo_play
-	install
-elif [ "$1" = "update" ]; then
-	logo_play
-	kaf
-	update
-else
-if [ ! -f ./libs/tgcli ]; then
-    echo "tgcli not found"
-    echo "Run $0 install"
-    exit 1
-fi
-	print_logo
-	kaf
-	logo_play
 	#sudo service redis-server restart
 	./libs/tgcli -s ./bot/bot.lua $@
 fi
+
