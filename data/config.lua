c228874c74b106b1fc3719d997ef154ca4339e9e
KafTeam <31245237+KafTeam@users.noreply.github.com>
2017-08-22 18:18:43 +0430

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,16 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "kaf"
+  },
+  info_text = "kaf V 2.1\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    100481835,
+  }
+}
+return _
+end
